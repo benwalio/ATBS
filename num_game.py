59ea0703f5c38e8d054db6e73a755fd0159166fb
Ben Wallace <ben@lup-ben.phi.lup.ie>
2017-11-09 15:39:40 -0500

    committing because it said to

--- num_game.py
+++ num_game.py
@@ -1,1 +1,26 @@
 # create a number game
+
+from random import randint
+import sys
+
+secret_num = randint(1,50)
+guess = 0
+guess_count = 0
+
+while guess <> secret_num:
+   guess_count += 1
+   guess = raw_input("What is your guess? ")
+   try:
+      guess = int(guess)
+   except:
+      print("that isn't a number -_-\nyou lose")
+      sys.exit()
+
+   if guess > secret_num:
+      print("Too high")
+      continue
+   elif guess < secret_num:
+      print("Too low")
+      continue
+   else:
+      print("correct! You won in " + str(guess_count) + " guesses!")
